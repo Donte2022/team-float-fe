56723a58f163c5e438e6cac31166c773d72a5f7b
joseph <jtownley684@gmail.com>
2022-11-05 10:16:06 -0500

    service wip

--- src/app/Service/main-page.service.ts
+++ src/app/Service/main-page.service.ts
@@ -10,9 +10,13 @@ export class MainPageService {
   //TODO add unsub
   private rank :number
   $MainShoppingPageMessage = new BehaviorSubject<string>("")
-  $MainShoppingPageMessage = new BehaviorSubject<string>("")
-  $MainShoppingPageMessage = new BehaviorSubject<string>("")
-  $MainShoppingPageMessage = new BehaviorSubject<string>("")
+  $CategoryEditMessage = new BehaviorSubject<string>("")
+  $CategoryCreateMessage = new BehaviorSubject<string>("")
+  $ProductMessage = new BehaviorSubject<string>("")
+  $ProductCreateMessage = new BehaviorSubject<string>("")
+  $ProductEditMessage = new BehaviorSubject<string>("")
+  $PriceChangeCreateMessage = new BehaviorSubject<string>("")
+  $PriceChangeEditMessage = new BehaviorSubject<string>("")
 
   constructor(private http:HttpService) {
     this.rank = 0
