2a1b40a48f7367b35b2b7d305a97a4a8d85e71d6
Michael Hood <michael.brian.hood@gmail.com>
2022-11-10 15:54:26 -0600

    Re-added updated main-page-component code to fix broken navbar

--- src/app/components/Mainpage/main-page/main-page.component.ts
+++ src/app/components/Mainpage/main-page/main-page.component.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MainPageService} from "../../../services/main-page.service";
+import {AccountService} from "../../../services/account.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
+  showAccountList: boolean = false
+  showMyAccount: boolean = false
 
-  shopingpageScreen : boolean = false
+  sub1: Subscription
+  sub2: Subscription
 
-  constructor(private MainPageService: MainPageService) {
-    this.MainPageService.$mainShoppingpageScreen.subscribe(value => {this.shopingpageScreen = value})
+  constructor(private accountService: AccountService, private mainPageService: MainPageService) {
+    this.sub1 = this.accountService.$showAccountList.subscribe(showAccountList => this.showAccountList = showAccountList)
+    this.sub2 = this.accountService.$showMyAccount.subscribe(showMyAccount => this.showMyAccount = showMyAccount)
   }
 
   ngOnInit(): void {
 
   }
 
-  onshopping () {
-    this.MainPageService.setMainShoppingPageScreen(true)
+  ngOnDestroy() {
+    this.sub1.unsubscribe()
+    this.sub2.unsubscribe()
+  }
+
+  onShopping () {
+    this.mainPageService.setMainShoppingPageScreen(true)
   }
 
 }
